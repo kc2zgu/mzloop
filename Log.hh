1c0a33947586f5b99e853aced41fee9b8b6ee1ae
Stephen Cavilia <sac@atomicradi.us>
2022-05-20 23:59:57 -0400

    Log.hh missing from previous commit

--- Log.hh
+++ Log.hh
@@ -5,10 +5,39 @@
 
 namespace mzloop
 {
+    enum LogLevel
+    {
+        Trace,
+        Message,
+        Warning,
+        Error
+    };
+
     class Log
     {
     public:
-        static void Message(std::string text);
+        static void Trace(std::string text)
+            {
+                Add(LogLevel::Trace, text);
+            }
+
+        static void Message(std::string text)
+            {
+                Add(LogLevel::Message, text);
+            }
+
+        static void Warning(std::string text)
+            {
+                Add(LogLevel::Warning, text);
+            }
+
+        static void Error(std::string text)
+            {
+                Add(LogLevel::Error, text);
+            }
+
+    protected:
+        static void Add(LogLevel level, std::string text);
     };
 };
 
